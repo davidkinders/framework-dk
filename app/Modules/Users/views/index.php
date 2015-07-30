40be05022d24433e8e0fe1cc0ecde7d0344d8f09
davidkinders <david@d-vision.be>
2015-07-30 18:45:47 +0200

    Initial commit

--- app/Modules/Users/views/index.php
+++ app/Modules/Users/views/index.php
@@ -15,13 +15,13 @@ $table->dataTable(true);
 $box = new Box();
 $box->title = "All users";
 $box->body = $table->render();
-$box->witdh = 6; // min 2 max 12
+$box->witdh = 8; // min 2 max 12
 $box->color = "info";
 $box->addTool('<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>');
 $box->border = true;
 ?>
 
 <div class="row">
-    <div class="col-md-3"></div>
+    <div class="col-md-2"></div>
     <?=$box->render();?>
 </div>
